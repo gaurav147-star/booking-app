986a6a11875ad5e21696af996dd7d5cc2267fb16
gaurav147-star <gauravgupta9158@gmail.com>
2022-05-29 21:21:58 +0530

    header completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/home/Home'
+import Hotel from './pages/hotel/Hotel'
+import List from './pages/list/List'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>GAURAV</h1>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter >
+  )
 }
 
-export default App;
+export default App
