a527c9588b24191527c272160cad23572e382f30
gaurav147-star <gauravgupta9158@gmail.com>
2022-07-07 10:56:35 +0530

    some bug fixed

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -8,33 +8,25 @@ const FeaturedProperties = () => {
 
   return (
     <div className="fp">
-      {loading ? (
-        "Loading"
-      ) : (
-        <>
-          {data.map((item) => (
-            <div className="fpItem" key={item._id}>
-              <LazyLoadImage
-                effect="blur"
-                src={item.photos[0]}
-                alt=""
-                className="fpImg"
-              />
-              <span className="fpName">{item.name}</span>
-              <span className="fpCity">{item.city}</span>
-              <span className="fpPrice">
-                Starting from ${item.cheapestPrice}
-              </span>
-              {item.rating && (
-                <div className="fpRating">
-                  <button>{item.rating}</button>
-                  <span>Excellent</span>
-                </div>
-              )}
+      {data.map((item) => (
+        <div className="fpItem" key={item._id}>
+          <LazyLoadImage
+            effect="blur"
+            src={item.photos[0]}
+            alt=""
+            className="fpImg"
+          />
+          <span className="fpName">{item.name}</span>
+          <span className="fpCity">{item.city}</span>
+          <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+          {item.rating && (
+            <div className="fpRating">
+              <button>{item.rating}</button>
+              <span>Excellent</span>
             </div>
-          ))}
-        </>
-      )}
+          )}
+        </div>
+      ))}
     </div>
   );
 };
