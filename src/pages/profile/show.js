1b284620dc22e3abdf550eca005a2a394d92d817
gaurav147-star <gauravgupta9158@gmail.com>
2022-07-10 17:41:39 +0530

    changes in admin and profile pages

--- src/pages/profile/show.js
+++ src/pages/profile/show.js
@@ -1,19 +1,84 @@
-import React from 'react'
-import Logo from '../../assets/lo.png'
-const Show = () => {
-  return (
-    <>
-    <div className='container emp-profile'>
-        <form action="">
-            <div className="row">
-                <div className="col-md-4">
-                    <img src={Logo} alt="" srcset="" />
-                </div>
-            </div>
-        </form>
-    </div>
-    </>
-  )
-}
+import React, { useContext } from "react";
+import "./show.scss";
+import Image from "../../assets/me.jpg";
+import { AuthContext } from "../../context/AuthContext";
 
-export default Show
+const Show = () => {
+
+  const { user } = useContext(AuthContext);
+//   console.log(user);
+
+  return (
+    <div className="profile">
+      <div className="showContainer">
+        <div className="profile_img">
+          <img src={Image} alt="" style={{ width: "300px" }} />
+        </div>
+        <div className="profile_details">
+          <div className="showCont">
+            <div className="showpcont">
+              <span>Full Name</span>
+              <input
+                type="text"
+                value={user.name}
+                className="lInput"
+                disabled
+              />
+            </div>
+            <div className="showpcont">
+              <span>Username</span>
+              <input
+                type="text"
+                value={user.username}
+                
+                className="lInput"
+                disabled
+              />
+            </div>
+            <div className="showpcont">
+              <span>Email</span>
+              <input
+                type="text"
+                value={user.email}
+                
+                className="lInput"
+                disabled
+              />
+            </div>
+
+            <div className="showpcont">
+              <span>Country</span>
+              <input
+                type="text"
+                value={user.country}
+                className="lInput"
+                disabled
+              />
+            </div>
+            <div className="showpcont">
+              <span>City</span>
+              <input
+                type="text"
+                value={user.city}
+                
+                className="lInput"
+                disabled
+              />
+            </div>
+            <div className="showpcont">
+              <span>Phone Number</span>
+              <input
+                type="number"
+                value={user.phone}
+                className="lInput"
+                disabled
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Show;
